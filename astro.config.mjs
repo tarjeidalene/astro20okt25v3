9541ffeeb2f10f27718778765e048265177e08ec
tarjeidalene <173668059+tarjeidalene@users.noreply.github.com>
2025-10-20 11:16:34 +0200

    Start (astro, adapter, AstroDB)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [db()]
+});
